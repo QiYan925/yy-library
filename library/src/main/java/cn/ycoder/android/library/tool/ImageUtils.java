c225d85ea891d958d5e7beb03950f01d20508bd2
QiYan <yu_bo925@163.com>
2017-07-27 14:16:39 +0800

    修正：增加清理Aroute历史访问

--- library/src/main/java/cn/ycoder/android/library/tool/ImageUtils.java
+++ library/src/main/java/cn/ycoder/android/library/tool/ImageUtils.java
@@ -13,21 +13,26 @@ import android.support.v4.graphics.drawable.DrawableCompat;
  */
 
 public class ImageUtils {
-    public static Drawable tintDrawable(Drawable drawable, ColorStateList colorStateList) {
-        Drawable drawable1 = DrawableCompat.wrap(drawable);
-        DrawableCompat.setTintList(drawable1, colorStateList);
-        return drawable1;
-    }
 
-    public static Drawable tintDrawable(Drawable drawable, @ColorInt int color) {
-        Drawable drawable1 = DrawableCompat.wrap(drawable);
-        DrawableCompat.setTintList(drawable1, ColorStateList.valueOf(color));
-        return drawable1;
-    }
+  public static Drawable tintDrawable(Drawable drawable, ColorStateList colorStateList) {
+    Drawable drawable1 = DrawableCompat.wrap(drawable);
+    DrawableCompat.setTintList(drawable1, colorStateList);
+    return drawable1;
+  }
 
-    public static int getDrawableResource(String drawableName,@DrawableRes int defaultRes) {
-        Context ctx = Utils.getContext();
-        int resId = ctx.getResources().getIdentifier(drawableName, "drawable", ctx.getPackageName());
-        return resId;
+  public static Drawable tintDrawable(Drawable drawable, @ColorInt int color) {
+    Drawable drawable1 = DrawableCompat.wrap(drawable);
+    DrawableCompat.setTintList(drawable1, ColorStateList.valueOf(color));
+    return drawable1;
+  }
+
+  public static int getDrawableResource(String drawableName, @DrawableRes int defaultRes) {
+    Context ctx = Utils.getContext();
+    try {
+      int resId = ctx.getResources().getIdentifier(drawableName, "drawable", ctx.getPackageName());
+      return resId;
+    } catch (Exception e) {
+      return defaultRes;
     }
+  }
 }
